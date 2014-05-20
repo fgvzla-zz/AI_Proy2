4a1d9b9e28a76f98d3819a411294c1723022da2b
UannaFF <anna.gomez.op@gmail.com>
2014-05-19 21:43:37 -0430

    Funciona Negamax y AlphaBeta

--- NegamaxAlphaBeta.cpp
+++ NegamaxAlphaBeta.cpp
@@ -20,57 +20,72 @@ struct hash_function_t : public tr1::hash<state_t> {
     }
 };
 
-class hash_table_t : public tr1::unordered_map<state_t, int, hash_function_t> {
+class hash_table_t : public tr1::unordered_map<state_t, pair<int, int>, hash_function_t> {
 
 };
 
 class NegamaxAlphaBeta{
 public:
-	hash_table_t tablaNegros;
-	hash_table_t tablaBlancos;
-
+	hash_table_t tablaExact;
+	hash_table_t tablaLower;
+	hash_table_t tablaUpper;
 	NegamaxAlphaBeta(state_t estado) {
 		//tabla.insert(make_pair(estado, estado.value()));
 	}
 
-	int busqueda(state_t n, int depth, bool turn, bool pass) {
+	int busqueda(state_t n, int depth, bool turn, bool pass, int alph, int beta) {
 		hash_table_t::iterator it;
-		vector<state_t> sucesores = n.succ(turn);
-		int alpha = -MAXVALUE, valor, segundo;
+		vector<state_t> sucesores;
 		bool paso = false;
-		//Calculando si se tranco.
+		int alphaO = alph, bestVal = -MAXVALUE, valor;
+		int alpha = alph;
+
+		//Se busca en las tablas de hash correspondientes.
+		it = tablaExact.find(n);
+		if(it != tablaExact.end()) {
+			if((*it).second.second >= depth) return (*it).second.first;
+		} else {
+			it = tablaLower.find(n);
+			if(it != tablaLower.end()) {
+				if((*it).second.second >= depth){
+					alpha = max(alpha, (*it).second.first);
+					if(alpha >= beta) return (*it).second.first;
+				}
+			} else {
+				it = tablaUpper.find(n);
+				if(it != tablaUpper.end()) {
+					if((*it).second.second >= depth) {
+						beta = min(beta, (*it).second.first);
+						if(alpha >= beta) return (*it).second.first;
+					}
+				}
+			}
+		}
+
+		sucesores = n.succ(turn);
 		if(sucesores[0] == n) paso = true;
 		if((paso & pass) | depth==0 | n.is_full()) {
 			if(turn) return n.value();
 			else return -n.value();
 		}
 		for(int i=0; i < sucesores.size(); i++) {
-			if(!turn) {
-				it = tablaBlancos.find(sucesores[i]);
-				if(it == tablaBlancos.end()) {
-					valor = -busqueda(sucesores[i], depth-1, !turn, paso);
-					alpha = max(alpha, valor);
-					//Se mete el alpha en la tabla de hash para cuando se repita, no tener que volver a hacerlo.
-					tablaBlancos.insert(make_pair(sucesores[i], valor));
-				} else {
-					segundo = (*it).second;
-					alpha = max(alpha, segundo);
-				}
-			} else {
-				it = tablaNegros.find(sucesores[i]);
-				if(it == tablaNegros.end()) {
-					valor = -busqueda(sucesores[i], depth-1, !turn, paso);
-					alpha = max(alpha, valor);
-					//Se mete el alpha en la tabla de hash para cuando se repita, no tener que volver a hacerlo.
-					tablaNegros.insert(make_pair(sucesores[i], valor));
-				} else {
-					segundo = (*it).second;
-					alpha = max(alpha, segundo);
-				}
-			}
+			valor = -busqueda(sucesores[i], depth-1, !turn, paso, -beta, -alpha);
+			bestVal = max(bestVal, valor);
+			alpha = max(alpha, valor);
+			if(alpha>=beta) break;
 		}
-		return alpha;
-	}
+		if(bestVal <= alphaO) {
+			//tablaUpper.erase(n);
+			tablaUpper.insert(make_pair(n, make_pair(bestVal, depth)));
+		} else if(bestVal >= beta) {
+			//tablaLower.erase(n);
+			tablaLower.insert(make_pair(n, make_pair(bestVal, depth)));
+		} else {
+			//tablaExact.erase(n);
+			tablaExact.insert(make_pair(n, make_pair(bestVal, depth)));
+		}
+		return bestVal;
 
+	}
 
 };
---
+++ mainNegamax.cpp
@@ -0,0 +1,71 @@
+/*
+ *  Copyright (C) 2012 Universidad Simon Bolivar
+ * 
+ *  Permission is hereby granted to distribute this software for
+ *  non-commercial research purposes, provided that this copyright
+ *  notice is included with any such distribution.
+ *  
+ *  THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ *  EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE
+ *  SOFTWARE IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU
+ *  ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+ *  
+ *  Blai Bonet, bonet@ldc.usb.ve
+ *
+ *  Last revision: 11/08/2012
+ *
+ */
+
+
+#include "Negamax.cpp"
+#include <iostream>
+
+
+using namespace std;
+
+int main(int argc, const char **argv) {
+    state_t state;
+    std::vector<state_t> v;
+    cout << "Principal variation:" << endl;
+    int lim = atoi(argv[1]);
+    bool player = false, pass = false;
+    int i;
+    for(i = 0; i < lim ; ++i ) {
+        player = i % 2 == 0; // black moves first!
+        int pos = PV[i];
+        cout << state;
+        cout << (player ? "Black" : "White")
+             << " moves at pos = " << pos << (pos == 36 ? " (pass)" : "")
+             << endl;
+        (pos == 36 ? pass = true : pass = false);
+        state = state.move(player, pos);
+        cout << "Board after " << i+1 << (i == 0 ? " ply:" : " plies:") << endl;
+
+    }
+    cout << state;
+    //cout << "Value of the game = " << state.value() << endl;
+    //cout << "#bits per state = " << sizeof(state) * 8 << endl;
+    Negamax nab = Negamax(state);
+    cout << state.value() << endl;
+    cout << "Lo que dio: " << nab.busqueda(state, 34 - lim-1, !player, pass) << "\n";
+    /*Negamax nei = Negamax(state);
+    cout << state.value() << endl;
+    cout << "Lo que dio: " << nei.busqueda(state, 34 - lim-1, !player, pass) << "\n";*/
+
+    /*if( argc > 1 ) {
+        int n = atoi(argv[1]);
+        cout << endl << "Apply " << n << " random movements at empty board:";
+        state = state_t();
+        for( int i = 0; i < n; ++i ) {
+            bool player = i % 2 == 0; // black moves first
+            int pos = state.get_random_move(player);
+            state = state.move(player, pos);
+            cout << " " << pos;
+        }
+        cout << endl << state;
+    }*/
+
+    return 0;
+}
