b40b0b02b61132dd3799a54d9df0f50330cfff77
UannaFF <anna.gomez.op@gmail.com>
2014-05-22 11:58:02 -0430

    NegaScout Listo, falta Scout c:

--- mainNegaScout.cpp
+++ mainNegaScout.cpp
@@ -3,16 +3,60 @@
 #include "othello_cut.h"
 #include <iostream>
 using namespace std;
+struct stored_info_t {
+    // [information to be stored in hash table]
+    int infor_;
+    stored_info_t() { } // need at least one ctor without arguments
+    int infor() const { return infor_; }
+    void setInfo(int profundo){
+        infor_ = profundo;
+    }
+};
 
+struct hash_function_t : public tr1::hash<state_t> {
+    size_t operator()(const state_t &state) const {
+        return state.hash();
+    }
+};
+
+class hash_table_t : public tr1::unordered_map<state_t, pair<int, int>, hash_function_t> {
+
+};
 class NegaScout{
 public:
+    hash_table_t tablaExact;
+    hash_table_t tablaLower;
+    hash_table_t tablaUpper;
     NegaScout(){}
 
     int busqueda(state_t nod, int depth, int alpha, int beta, bool turn, bool pass){
         vector<state_t> sucesores = nod.succ(turn);
         bool paso = false;
         int t;
-        int m = alpha;
+        int m = -MAXVALUE, n = beta;
+        hash_table_t::iterator it;
+
+        //Se busca en las tablas de hash correspondientes.
+        it = tablaExact.find(nod);
+        if(it != tablaExact.end()) {
+            if((*it).second.second >= depth) return (*it).second.first;
+        } else {
+            it = tablaLower.find(nod);
+            if(it != tablaLower.end()) {
+                if((*it).second.second >= depth){
+                    m = max(m, (*it).second.first);
+                    if(m >= beta) return (*it).second.first;
+                }
+            } else {
+                it = tablaUpper.find(nod);
+                if(it != tablaUpper.end()) {
+                    if((*it).second.second >= depth) {
+                        beta = min(beta, (*it).second.first);
+                        if(m >= beta) return (*it).second.first;
+                    }
+                }
+            }
+        }
 
         if(sucesores[0] == nod) paso = true;
         if((paso & pass) | depth==0 | nod.is_full()) {
@@ -21,7 +65,7 @@ public:
         }
         
 
-        for(int i = 0; i < sucesores.size();i++){
+        /*for(int i = 0; i < sucesores.size();i++){
             if(i != 0){
                 t = -busqueda(sucesores[i],depth-1,-m-1,-m,!turn,paso);
                 if(m < t < beta){
@@ -29,12 +73,36 @@ public:
                 }
 
             }else{
-                t = -busqueda(sucesores[i],depth-1, -beta,-alpha, !turn,paso);    
+                t = -busqueda(sucesores[i],depth-1, -beta,-m, !turn,paso);    
             }
             m = max(m,t);
             if(m >= beta){
                 break;
             }
+        }*/
+
+        for(int i = 0; i < sucesores.size(); i++){
+            t = - busqueda(sucesores[i], depth -1, -n,-max(m,alpha),!turn,paso);
+            if(t>m){
+                if(n==beta || depth < 3 || t>=beta ){
+                    m = t;
+                }else{
+                    m = -busqueda(sucesores[i],depth-1,-beta,-t,!turn,paso);
+                }
+            }
+            if(m>=beta) return m;
+            n=max(alpha,m)+1;
+        }
+
+        if(m <= alpha) {
+            tablaUpper.erase(nod);
+            tablaUpper.insert(make_pair(nod, make_pair(m, depth)));
+        } else if(m >= beta) {
+            tablaLower.erase(nod);
+            tablaLower.insert(make_pair(nod, make_pair(m, depth)));
+        } else {
+            tablaExact.erase(nod);
+            tablaExact.insert(make_pair(nod, make_pair(m, depth)));
         }
 
         return m;
